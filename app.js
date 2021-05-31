b1fdd028770aac9e121e10896f822c3498f1b1d8
Gultaj Amanova <gultac.amanova@gmail.com>
2021-05-31 12:04:47 +0400

    first part of JS finish

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const jobsCardContainer = document.querySelector('.container');
+
 const jobsAnouncements = [
   {
     title: 'Senior Frontend Developer',
@@ -55,3 +57,36 @@ const jobsAnouncements = [
     companyName: 'Shortly'
   }
 ];
+
+jobsAnouncements.forEach(jobsAnouncement => {
+  
+  const cardsOfJob = `
+  <div class="job-card">
+  <div class="job-card__cover">
+          <img src="./images/${jobsAnouncement.coverPhoto}" alt="" />
+        </div>
+        <div class="job-card__description">
+          <div class="job-card__description-top">
+            <span class="job-card__company-name">${jobsAnouncement.companyName}</span>
+            <span class="job-card__badge job-card__badge--new">${jobsAnouncement.new}</span>
+            <span class="job-card__badge job-card__badge--featured"
+              >${jobsAnouncement.featured}</span>
+          </div>
+          <p class="job-card__title">${jobsAnouncement.title}</p>
+          <div class="job-card__description-bottom">
+            <span class="job-card__published">${jobsAnouncement.published}</span>
+            <span class="job-card__shift">${jobsAnouncement.shift}</span>
+            <span class="job-card__location">${jobsAnouncement.place}</span>
+          </div>
+        </div>
+        <div class="job-card__tags">
+          <span class="job-card__tag">Frontend</span>
+          <span class="job-card__tag">HTML</span>
+          <span class="job-card__tag">CSS</span>
+          <span class="job-card__tag">JavaScript</span>
+          <span class="job-card__tag">Senior</span>
+        </div>
+      </div> `
+
+        jobsCardContainer.insertAdjacentHTML('beforeend', cardsOfJob)
+})
