c8e1387f5ddb5a25e63561a460b691f7e3fd5536
Gultaj Amanova <gultac.amanova@gmail.com>
2021-05-31 11:50:21 +0400

    setup of project

---
+++ app.js
@@ -0,0 +1,57 @@
+const jobsAnouncements = [
+  {
+    title: 'Senior Frontend Developer',
+    coverPhoto: 'faceit.svg',
+    published: '1d ago',
+    shift: 'Full Time',
+    place: 'USA only',
+    tags: ['Frontend', 'HTML', 'CSS', 'JavaScript', 'Senior'],
+    featured: true,
+    new: true,
+    companyName: 'FaceIt'
+  },
+  {
+    title: 'Junior Frontend Developer',
+    coverPhoto: 'photosnap.svg',
+    published: '1d ago',
+    shift: 'Full Time',
+    place: 'USA only',
+    tags: ['Frontend', 'HTML', 'CSS', 'JavaScript', 'Senior'],
+    featured: true,
+    new: true,
+    companyName: 'Photosnap'
+  },
+  {
+    title: 'Full Stack Developer',
+    coverPhoto: 'manage.svg',
+    published: '1d ago',
+    shift: 'Full Time',
+    place: 'USA only',
+    tags: ['Fullstack', 'HTML', 'Python', 'React'],
+    featured: true,
+    new: true,
+    companyName: 'Manage'
+  },
+  {
+    title: 'Junior Frontend Developer',
+    coverPhoto: 'myhome.svg',
+    published: '1d ago',
+    shift: 'Full Time',
+    place: 'USA only',
+    tags: ['Frontend', 'HTML', 'Python', 'JavaScript', 'Senior'],
+    featured: false,
+    new: false,
+    companyName: 'MyHome'
+  },
+  {
+    title: 'Shortly Frontend Developer',
+    coverPhoto: 'shortly.svg',
+    published: '1d ago',
+    shift: 'Full Time',
+    place: 'USA only',
+    tags: ['Frontend', 'HTML', 'Python', 'JavaScript', 'Senior'],
+    featured: false,
+    new: false,
+    companyName: 'Shortly'
+  }
+];
